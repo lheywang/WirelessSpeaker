68aee57347c647d3bc921248f3a6085425d6e59a
lheywang <leonard.heywang@gmail.com>
2024-09-26 15:25:58 +0200

    Removed useless operations

--- src/Drivers/I2C/I2C_Engine.cpp
+++ src/Drivers/I2C/I2C_Engine.cpp
@@ -120,7 +120,6 @@ int I2C_Read(I2C_Bus *I2C, int Address, int Register, int *Payload, int Size, in
         // Match the wanted datasize.
         if (DataSize == 8)
         {
-            i2c_smbus_write_byte(I2C->I2C_file, i + Register);
             res += i2c_smbus_read_byte(I2C->I2C_file);
         }
         else if (DataSize == 16)
