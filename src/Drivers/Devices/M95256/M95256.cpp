18f236ecb5476308bbd999e2a308e7d70b586339
lheywang <leonard.heywang@gmail.com>
2024-10-12 13:30:32 +0200

    Added EEPROM Driver. Need to be tested !

--- src/Drivers/Devices/M95256/M95256.cpp
+++ src/Drivers/Devices/M95256/M95256.cpp
@@ -9,6 +9,13 @@
  *
  */
 
+#include "../../SPI/SPI.hpp"
+#include "M95256.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <cerrno>
+#include <cstring>
+
 // ==============================================================================
 // PRIVATE DEFINES
 // ==============================================================================
@@ -18,3 +25,160 @@
 #define WRSR 0x01  // No following data
 #define READ 0x03  // 2 bytes address page
 #define WRITE 0x02 // 2 bytes address page
+
+// ==============================================================================
+// FUNCTIONS
+// ==============================================================================
+
+M95256::M95256(const SPI_Bus *SPI)
+{
+    this->SPI = *SPI;
+    return;
+}
+
+M95256::~M95256()
+{
+    return;
+}
+
+int M95256::WriteEnable()
+{
+    int buf[3] = {0};
+    int res = 0;
+
+    buf[0] = WREN;
+
+    res = SPI_Transfer(&this->SPI, buf, buf, 1);
+
+    if (res != 0)
+        return -1;
+    return 0;
+}
+
+int M95256::WriteDisable()
+{
+    int buf[3] = {0};
+    int res = 0;
+
+    buf[0] = WRDI;
+
+    res = SPI_Transfer(&this->SPI, buf, buf, 1);
+
+    if (res != 0)
+        return -1;
+    return 0;
+}
+
+int M95256::ReadStatus(int *const WriteProtectStatus,
+                       int *const ProtectedBlock,
+                       int *const WriteEnable,
+                       int *const WriteInProgress)
+{
+    int buf[3] = {0};
+    int res = 0;
+
+    buf[0] = RDSR;
+
+    res = SPI_Transfer(&this->SPI, buf, buf, 1);
+
+    if (res != 0)
+        return -1;
+
+    *WriteProtectStatus = (buf[0] & 0x80) >> 7;
+    *ProtectedBlock = (buf[0] & 0x0C) >> 2;
+    *WriteEnable = (buf[0] & 0x02) >> 1;
+    *WriteInProgress = buf[0] & 0x01;
+
+    return 0;
+}
+
+int M95256::WriteStatus(const int WriteProtectStatus,
+                        const int ProtectedBlock)
+{
+    if ((0 > ProtectedBlock) | (ProtectedBlock > 0x03))
+        return -1;
+
+    int buf[3] = {0};
+    int res = 0;
+
+    buf[0] = (bool)WriteProtectStatus;
+    buf[0] = buf[0] << 4 | ProtectedBlock;
+    buf[0] = buf[0] << 2;
+
+    res = SPI_Transfer(&this->SPI, buf, buf, 1);
+
+    if (res != 0)
+        return -2;
+    return 0;
+}
+
+int M95256::Read(const int Address, int *const Data, const int Len)
+{
+    if ((0 > Address) | (Address > MAX_ADDRESS))
+        return -1;
+    if ((Address + Len) > MAX_ADDRESS)
+        return -2;
+
+    int res = 0;
+    int *buf = (int *)malloc((sizeof(int) * (Len + 3)));
+    if (buf == 0)
+    {
+        std::cerr << "[ M95256 ][ Read ] Could not allocate memory for read operation "
+                  << strerror(errno)
+                  << std::endl;
+        return -1;
+    }
+
+    memset(buf, 0x00, (sizeof(int) * (Len + 1)));
+    buf[0] = READ;
+    buf[1] = Address & 0xFF00;
+    buf[2] = Address & 0x00FF;
+
+    res = SPI_Transfer(&this->SPI, buf, buf, 1);
+
+    for (int i = 0; i < Len; i++)
+        Data[i] = buf[i + 3];
+
+    if (res != 0)
+    {
+        free(buf);
+        return -3;
+    }
+
+    free(buf);
+    return 0;
+}
+
+int M95256::Write(const int Address, int *const Data, const int Len)
+{
+    if ((0 > Address) | (Address > MAX_ADDRESS))
+        return -1;
+    if ((Address + Len) > MAX_ADDRESS)
+        return -2;
+
+    int res = 0;
+    int *buf = (int *)malloc((sizeof(int) * (Len + 3)));
+    if (buf == 0)
+    {
+        std::cerr << "[ M95256 ][ Write ] Could not allocate memory for read operation "
+                  << strerror(errno)
+                  << std::endl;
+        return -1;
+    }
+
+    buf[0] = WRITE;
+    buf[1] = Address & 0xFF00;
+    buf[2] = Address & 0x00FF;
+    memcpy(&buf[3], Data, Len * sizeof(int));
+
+    res = SPI_Transfer(&this->SPI, buf, buf, 1);
+
+    if (res != 0)
+    {
+        free(buf);
+        return -2;
+    }
+
+    free(buf);
+    return 0;
+}
--- src/Drivers/Devices/M95256/M95256.hpp
+++ src/Drivers/Devices/M95256/M95256.hpp
@@ -8,6 +8,10 @@
  * @copyright Copyright (c) 2024
  *
  */
+#pragma once
+
+// type include
+#include "../../SPI/SPI.hpp"
 
 // ==============================================================================
 // PUBLIC DEFINES
@@ -18,3 +22,100 @@
 #define SOFT_WP_UPPER_QUARTER ((0 << 1) | 1)
 #define SOFT_WP_UPPER_HALF ((1 << 1) | 0)
 #define SOFT_WP_FULL ((1 << 1) | 1)
+
+// ==============================================================================
+// IC CLASS FUNCTIONS
+// ==============================================================================
+
+class M95256
+{
+private:
+    SPI_Bus SPI;
+
+public:
+    /**
+     * @brief Constructor for the M95256 class.
+     *
+     * @param SPI An SPI object to be used as base
+     */
+    M95256(const SPI_Bus *SPI);
+
+    /**
+     * @brief Destructor for the M95256 class.
+     *
+     */
+    ~M95256();
+
+    /**
+     * @brief Enable the ability to write on the chip. Warning : The physical pin can overhide this value.
+     *
+     * @return  0 : OK
+     * @return -1 : IOCTL error.
+     */
+    int WriteEnable();
+
+    /**
+     * @brief Disable the write ability. This is also done after each write.
+     *
+     * @return  0 : OK
+     * @return -1 : IOCTL error.
+     */
+    int WriteDisable();
+
+    /**
+     * @brief Read back the status register.
+     *
+     * @param WriteProtectStatus Return the status of the write protection, in term of software and hardware.
+     * @param ProtectedBlock Return the protected block.
+     * @param WriteEnable Return the status of the write enable bit.
+     * @param WriteInProgress Return the status of the write engine.
+     *
+     * @return  0 : OK
+     * @return -1 : IOCT error.
+     */
+    int ReadStatus(int *const WriteProtectStatus,
+                   int *const ProtectedBlock,
+                   int *const WriteEnable,
+                   int *const WriteInProgress);
+
+    /**
+     * @brief Write the status register
+     *
+     * @param WriteProtectStatus Write the protection mode register
+     * @param ProtectedBlock Write the protected block values
+     *
+     * @return 0 : OK
+     * @return -1 : Invalid Protected block value
+     * @return -2 : IOCTL error.
+     */
+    int WriteStatus(const int WriteProtectStatus,
+                    const int ProtectedBlock);
+
+    /**
+     * @brief Read data of the EEPROM
+     *
+     * @param Adresss Address of the first byte to be read.
+     * @param Data A pointer to a list of Len elements to store the output.
+     * @param Len The number of bytes to read.
+     *
+     * @return  0 : OK
+     * @return -1 : Invalid address
+     * @return -2 : Invalid Len (May be triggered if Address + Len > MAX_ADDRESS)
+     * @return -3 : IOCTL error.
+     */
+    int Read(const int Address, int *const Data, const int Len);
+
+    /**
+     * @brief Write content to EEPROM. Only possible if write has been enabled !
+     *
+     * @param Address Adress of the first byte to be wrote
+     * @param Data A pointer to a list of elements to be wrote.
+     * @param Len The number of bytes to write.
+     *
+     * @return  0 : OK
+     * @return -1 : Invalid address
+     * @return -2 : Invalid Len (May be triggered if Address + Len > MAX_ADDRESS)
+     * @return -3 : IOCTL error.
+     */
+    int Write(const int Address, int *const Data, const int Len);
+};
