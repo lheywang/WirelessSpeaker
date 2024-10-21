cf596fb10b117234f076d7f97ac7dc79172993fd
lheywang <leonard.heywang@gmail.com>
2024-10-21 22:26:35 +0200

    Updated workflow

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #bin/
 echo "I will now install and set up everything to make the compilation possible..."
 
-apt-get install -y build-essential libc6-dev make doxygen ssh libi2c-dev device-tree-compiler g++-aarch64-linux-gnu cmake
+apt-get install -y build-essential libc6-dev make doxygen ssh libi2c-dev device-tree-compiler g++-aarch64-linux-gnu cmake python3.11
 cd ../tools/ && git clone https://github.com/raspberrypi/utils.git
 cd ../tools/utils && cmake . && sudo make install
 echo "If no errors occureds, all tools are installed sucessfully"
