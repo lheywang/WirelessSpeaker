9497a23bebbe6b6f81d373697498776561422808
lheywang <leonard.heywang@gmail.com>
2024-11-05 11:41:57 +0100

    Started statistics computations

--- tools/electronic-design-tools/SpicePlotter.py
+++ tools/electronic-design-tools/SpicePlotter.py
@@ -9,13 +9,23 @@
 # ===============================================================================
 
 import os
-
-# from PyQSPICE import clsQSPICE as pqs # type: ignore
-from pick import pick  # type: ignore
-import cudf
 import numpy as np
 import time
 from tqdm import tqdm
+import dask
+dask.config.set({"dataframe.backend": "cudf"})
+import dask.dataframe as dd
+
+try:
+    import cudf
+except:
+    print(
+        "Failed to import cudf...\nNow using pandas as replacement but with performance loss"
+    )
+    exit()
+
+
+# Starting execution chronometer
 cuts = time.time()
 
 # First, we need to replace every \t with a ,
@@ -23,7 +33,7 @@ cuts = time.time()
 # We do this for every file in the data folder.
 obj = os.scandir(os.getcwd() + "/data")
 files = []
-for entry in obj :
+for entry in obj:
     if entry.is_file():
         files.append(entry.name)
 
@@ -40,28 +50,68 @@ for entry in obj :
 # Load the CSV file
 # 21 columns !
 # The order shall be defined by the user when exporting ...
+print("Loading data into VRAM...")
 df = cudf.read_csv(f"temp/{files[0]}", sep=",")
 for file in tqdm(files[1:]):
-    df = cudf.concat([df, cudf.read_csv(f"temp/{file}", sep=",")], axis=0)
+    df = cudf.concat(
+        [df, cudf.read_csv(f"temp/{file}", sep=",").astype("float32")], axis=0
+    )
 
-# Convert all columns to their Amplititude and Phase. 
+# Convert all columns to their Amplititude and Phase.
+print("Preparing data for future treatement...")
 MagPhi = cudf.DataFrame()
 MagPhi["Frequency"] = df["Frequency"]
-col = ["outr+high","outr-high","outr+mediums","outr-mediums","outl+high","outl-high","outl+mediums","outl-mediums","out+bass","out-bass"]
-for column in col:
-    MagPhi[f"V({column})"] = 20 * np.log10(np.abs(np.sqrt(np.power(df[f"V({column})"], 2) + np.power(df[f"iV({column})"], 2))))
-    MagPhi[f"P({column})"] =  np.arctan2(df[f"iV({column})"], df[f"V({column})"]) * 180 / np.pi
+col = [
+    "outr+high",
+    "outr-high",
+    "outr+mediums",
+    "outr-mediums",
+    "outl+high",
+    "outl-high",
+    "outl+mediums",
+    "outl-mediums",
+    "out+bass",
+    "out-bass",
+]
+for column in tqdm(col):
+    MagPhi[f"V({column})"] = 20 * np.log10(
+        np.abs(
+            np.sqrt(np.power(df[f"V({column})"], 2) + np.power(df[f"iV({column})"], 2))
+        )
+    )
+    MagPhi[f"P({column})"] = (
+        np.arctan2(df[f"iV({column})"], df[f"V({column})"]) * 180 / np.pi
+    )
 
-# Processing data. 
-chunk_size = 3822
-# Then we process the data, per chunk. Each chunk correspond a step with all studies frequencies.
-MMM = cudf.DataFrame()
-for i in tqdm(range(0, len(MagPhi), chunk_size)):
-    chunk = MagPhi.iloc[i:i+chunk_size].copy()
-    MMM["Frequency"] = chunk["Frequency"]
-    MMM["Median"] += chunk[""]
+# Too slow...
+# Idea : Group by frequencies, for example 1-1.5, 2.5 ...
+# We loss a bit of precision but we don't really care here since the goal is to prompt an idea of the data, not the exact value !
+
+print("Computing statistics per frequencies...")
+Gr = MagPhi.groupby("Frequency")
+Means = cudf.Series([])
+Min = cudf.Series([])
+Max = cudf.Series([])
+STD = cudf.Series([])
+
+for freq, group_df in tqdm(Gr):
+    cudf.concat([Means, cudf.Series([group_df.mean()])])
+    cudf.concat([Min, cudf.Series([group_df.min()])])
+    cudf.concat([Max, cudf.Series([group_df.max()])])
+    cudf.concat([STD, cudf.Series([group_df.std()])])
+
+print(Means.head)
+print(Min.head)
+print(Max.head)
+print(STD.head)
+
+
+
+# for freq in tqdm(Freqs):
+#     Stats._concat([freq.mean(), freq.min(), freq.max(), freq.std()])
+# print(Stats)
+
 
 cutf = time.time()
 input("Press enter to exit...")
 print(f"Time elapsed : {cutf - cuts}")
-
